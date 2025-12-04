d8872b71939259d4d869d256537535e0cb9edd09
PachecoGit1306 <0933849@my.scccd.edu>
2025-12-03 16:38:25 -0800

    Update player.m

--- player.m
+++ player.m
@@ -1,1 +1,23 @@
+classdef player
+    properties
+        name
+        team
+        hits
+        walks
+        atbats
+    end
 
+    properties (Dependent)
+        battingAvg
+        onBasePct
+    end
+
+    methods
+
+        function obj = player(name, team, hits, walks, atbats)
+            obj.name = name;
+            obj.team = team;
+            obj.hits = hits;
+            obj.walks = walks;
+            obj.atbats = atbats;
+        end
